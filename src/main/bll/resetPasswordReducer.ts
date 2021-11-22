7b13e31352ece234e8ee6af34dbc78c194a51c47
dzimentey <dzimentey@gmail.com>
2021-11-22 20:27:18 +0300

    feat: disable the send button while server response has not received

--- src/main/bll/resetPasswordReducer.ts
+++ src/main/bll/resetPasswordReducer.ts
@@ -7,6 +7,7 @@ type InitStateType = typeof initState;
 const initState = {
     info: '',
     error: '',
+    loading: false,
 };
 
 // reducer
@@ -16,21 +17,28 @@ export const resetPasswordReducer = (state = initState, action: ActionType): Ini
             return {...state, info: action.info};
         case "RESET-PASSWORD/SET-ERROR":
             return {...state, error: action.error};
+        case "RESET-PASSWORD/SET-LOADING":
+            return {...state, loading: action.loading};
         default:
             return state;
     }
 };
 
 // action
-type ActionType = ReturnType<typeof setResetPasswordInfoAC> | ReturnType<typeof setResetPasswordErrorAC>;
+type ActionType = ReturnType<typeof setResetPasswordInfoAC> | ReturnType<typeof setResetPasswordErrorAC>
+                                                            | ReturnType<typeof setResetPasswordLoadingAC>;
 
 export const setResetPasswordInfoAC = (info: string) => ({type: 'RESET-PASSWORD/SET-INFO', info} as const);
 
 export const setResetPasswordErrorAC = (error: string) => ({type: 'RESET-PASSWORD/SET-ERROR', error} as const);
 
+export const setResetPasswordLoadingAC = (loading: boolean) => ({type: 'RESET-PASSWORD/SET-LOADING', loading} as const);
 
 // thunk
 export const resetPasswordTC = (email: string, message: string) => (dispatch: Dispatch) => {
+
+    dispatch(setResetPasswordLoadingAC(true)); // disable send button
+
     cardsAPI.resetPassword(email, message)
         .then((res) => {
             dispatch(setResetPasswordInfoAC(res.data.info));
@@ -39,5 +47,8 @@ export const resetPasswordTC = (email: string, message: string) => (dispatch: Di
             let errorMessage = error.response?.data.error
             //console.log(errorMessage);
             dispatch(setResetPasswordErrorAC(errorMessage));
+        })
+        .finally(() => {
+            dispatch(setResetPasswordLoadingAC(false)); // enable send button
         })
 };
